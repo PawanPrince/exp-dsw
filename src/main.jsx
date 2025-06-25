7a2123e7225ed53ed98f3b589940a7f7d9ae8635
PawanPrince <pawanprince198@gmail.com>
2025-06-25 13:34:55 +0530

    new one4

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import "./global.css";
+import React, { Suspense } from 'react';
+import App from "./App";
+import Loader from './alltopics/16_createBrowserRouter/Loder'; // ✅ adjust the path if needed
 
-// const div = document.getElementById("root");
-
-// createRoot(div).render(
-
-//   <div>
-//     <h1>Hello world</h1>
-//     <h2>Java Script</h2>
-//   </div> );
-
-import App from "./App"; 
-
-// createRoot(div).render(<App></App>);
 const div = document.getElementById("root");
-createRoot(div).render(<App/>);
+
+createRoot(div).render(
+  <Suspense
+    fallback={
+      <div className="fixed inset-0 flex items-center justify-center bg-black text-white z-50">
+        <Loader />
+      </div>
+    }
+  >
+    <App />
+  </Suspense>
+);
